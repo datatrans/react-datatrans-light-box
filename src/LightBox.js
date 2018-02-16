f93d67563019b74df4fc74a46c4df8c6b751cc8b
Michael Raith <m.raith@netconomy.net>
2018-02-16 08:55:35 +0100

    fixssr Fix indentation

--- src/LightBox.js
+++ src/LightBox.js
@@ -2,18 +2,18 @@ import React, {Component} from 'react'
 // import PropTypes from 'prop-types'
 import PaymentPageFrame from './PaymentPageFrame'
 import {
-    filterProps, toUrlParams, parseUrl,
-    lockScrolling,
-    releaseLock
+  filterProps, toUrlParams, parseUrl,
+  lockScrolling,
+  releaseLock
 } from './utils'
 
 const rejectProps = [
-    'production', 'onCancelled', 'onLoaded', 'onError', 'version'
+  'production', 'onCancelled', 'onLoaded', 'onError', 'version'
 ]
 
 const buildUrl = (props, production) => production
-    ? 'https://payment.datatrans.biz/upp/jsp/upStart.jsp?' + toUrlParams(props)
-    : 'https://pilot.datatrans.biz/upp/jsp/upStart.jsp?' + toUrlParams(props)
+? 'https://payment.datatrans.biz/upp/jsp/upStart.jsp?' + toUrlParams(props)
+: 'https://pilot.datatrans.biz/upp/jsp/upStart.jsp?' + toUrlParams(props)
 
 const hasError = ev => typeof ev.data !== typeof undefined && ev.data.type === 'error'
 
@@ -21,59 +21,58 @@ const hasError = ev => typeof ev.data !== typeof undefined && ev.data.type === '
 export default class LightBox extends Component {
 
 
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.onMessage = this.onMessage.bind(this)
-        this.onCancelled = this.onCancelled.bind(this)
+    this.onMessage = this.onMessage.bind(this)
+    this.onCancelled = this.onCancelled.bind(this)
 
-        lockScrolling()
+    lockScrolling()
 
-        const urlParams = filterProps(this.props, rejectProps)
-        this.url = buildUrl(urlParams, this.props.production)
-        this.origin = parseUrl(this.url).origin
+    const urlParams = filterProps(this.props, rejectProps)
+    this.url = buildUrl(urlParams, this.props.production)
+    this.origin = parseUrl(this.url).origin
 
-        this.state = { visible: true}
+    this.state = { visible: true}
+  }
+
+  onMessage(ev) {
+    if(ev.origin !== this.origin) return
+    if(ev.data === 'cancel') return this.onCancelled()
+    if(ev.data === 'frameReady') return this.props.onLoaded()
+    if(hasError(ev)) return this.props.onError(ev.data)
+  }
+
+  onCancelled() {
+    this.setState({visible: false})
+    releaseLock()
+    this.props.onCancelled()
+  }
+
+  componentDidMount() {
+    if (typeof window !== 'undefined') {
+        const addListener = window.addEventListener || window.attachEvent
+        addListener('message', this.onMessage)
     }
+  }
 
-    onMessage(ev) {
-        if(ev.origin !== this.origin) return
-        if(ev.data === 'cancel') return this.onCancelled()
-        if(ev.data === 'frameReady') return this.props.onLoaded()
-        if(hasError(ev)) return this.props.onError(ev.data)
+  componentWillUnmount() {
+    if (typeof window !== 'undefined') {
+        const removeListener = window.removeEventListener || window.detachEvent
+        removeListener('message', this.onMessage)
     }
+    releaseLock()
+  }
 
-    onCancelled() {
-        this.setState({visible: false})
-        releaseLock()
-        this.props.onCancelled()
-    }
+  render() {
 
-    componentDidMount() {
-        if (typeof window !== 'undefined') {
-            const addListener = window.addEventListener || window.attachEvent
-            addListener('message', this.onMessage)
-        }
+    if(!this.state.visible) return <div> do not render me </div>
 
-    }
-
-    componentWillUnmount() {
-        if (typeof window !== 'undefined') {
-            const removeListener = window.removeEventListener || window.detachEvent
-            removeListener('message', this.onMessage)
-        }
-        releaseLock()
-    }
-
-    render() {
-
-        if(!this.state.visible) return <div> do not render me </div>
-
-        return <PaymentPageFrame
-        url={this.url}
-        showsPaymentPage={this.props.showsPaymentPage}
-        />
-    }
+    return <PaymentPageFrame
+      url={this.url}
+      showsPaymentPage={this.props.showsPaymentPage}
+    />
+  }
 
 }
 
@@ -95,11 +94,11 @@ export default class LightBox extends Component {
 // }
 
 LightBox.defaultProps = {
-    onCancelled() {},
-    onLoaded() {},
-    onError() {},
-    production: false,
-    theme: 'DT2015',
-    version: '1.0.2',
-    showsPaymentPage: true,
+  onCancelled() {},
+  onLoaded() {},
+  onError() {},
+  production: false,
+  theme: 'DT2015',
+  version: '1.0.2',
+  showsPaymentPage: true,
 }
