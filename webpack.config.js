7078c0d9069bcddd47e5f83346c6fc6e41a90622
Patrick Stadler <patrick.stadler@datatrans.ch>
2018-10-23 09:09:43 +0200

    fix case-sensitivity

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: '[name].js',
-    library: 'LightBox',
+    library: 'Lightbox',
     libraryTarget: 'umd',
     // umdNamedDefine: true
   },
