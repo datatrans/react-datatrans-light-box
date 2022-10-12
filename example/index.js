fe23a85eecb515727ea0f2be9d5801bf78690899
Nessie <694883+n3ssi3@users.noreply.github.com>
2022-10-12 09:29:31 +0200

    Support React 18 Strict Mode / useEffect (#24)

--- example/index.js
+++ example/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
-ReactDom.render(
-  <App />,
-  document.getElementById('app')
+createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
